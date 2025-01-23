83c48e20486b5e59533a1cf91a3cc0d06cecf9c4
steffenhove <steffenhove@gmail.com>
2025-01-23 20:22:34 +0100

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-       // classpath("com.android.tools.build:gradle:8.2.2")
+        // classpath("com.android.tools.build:gradle:8.2.2")
         //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath ("org.jetbrains.kotlin:kotlin-serialization:2.1.0")
         classpath ("com.android.tools:r8:8.7.18")
