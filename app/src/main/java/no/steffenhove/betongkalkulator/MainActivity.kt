7b08db3319fa4d210f8a6dee71d950f4236886a6
steffenhove <steffenhove@gmail.com>
2025-06-01 22:02:29 +0200

    Add files via upload

--- app/src/main/java/no/steffenhove/betongkalkulator/MainActivity.kt
+++ app/src/main/java/no/steffenhove/betongkalkulator/MainActivity.kt
@@ -3,8 +3,12 @@ package no.steffenhove.betongkalkulator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
